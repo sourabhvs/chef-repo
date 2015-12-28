b5fefcc8c6850fc3027da985a89d2ba04d9e8e79
pmprasad <pmprasad@qwinixtech.com>
2015-12-28 10:53:32 +0530

    update data bags

--- cookbooks/apache/recipes/vhosts.rb
+++ cookbooks/apache/recipes/vhosts.rb
@@ -1,7 +1,8 @@
 data_bag("vhosts").each do |site|
   site_data = data_bag_item("vhosts",site)
   app_name = site_data["appname"]
-  document_root = "/u01/apps/qwinix/#{app_name}/current/public/"
+  app_path = site_data["apppath"] 
+  document_root = "/u01/apps/qwinix/#{app_name}/#{app_path}"
   errolog = "/u01/logs/httpd/#{app_name}-error.log"
   customlog = "/u01/logs/httpd/#{app_name}-access.log"
 
@@ -13,12 +14,6 @@ data_bag("vhosts").each do |site|
       :port => site_data["port"],
       :server_name => site_data["servername"],
       :document_root => document_root,
-      :passenger_mod => site_data["passenger_mod"],
-      :passenger_ruby_home => site_data["passenger_ruby_home"],
-      :passenger_pool_size => site_data["passenger_pool_size"],
-      :passenger_pool_id => site_data["passenger_pool_id"],
-      :passenger_maxinstance_app => site_data["passenger_maxinstance_app"],  
-      :rails_env => site_data["rails_env"]
     )
    notifies :restart, "service[httpd]"
   end
---
+++ cookbooks/apache/recipes/vhosts_ror.rb
@@ -0,0 +1,62 @@
+execute 'delete conf.d/*.*' do
+     command 'rm -rf /etc/httpd/conf.d/*'
+end
+
+#Data bag for comman vhosts 
+data_bag("vhosts").each do |site|
+  site_data = data_bag_item("vhosts",site)
+  app_name = site_data["appname"]
+  app_path = site_data["apppath"]
+  document_root = "/u01/apps/qwinix/#{app_name}/#{app_path}"
+  errolog = "/u01/logs/httpd/#{app_name}-error.log"
+  customlog = "/u01/logs/httpd/#{app_name}-access.log"
+
+  template "/etc/httpd/conf.d/#{app_name}.conf" do
+    source "custom-vhosts.erb"
+    mode "0644"
+    variables(
+      :appname => site_data["appname"],
+      :port => site_data["port"],
+      :server_name => site_data["servername"],
+      :document_root => document_root,
+    )
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+end
+
+#Data bag for ROR application vhosts 
+data_bag("vhosts_ROR").each do |site|
+  site_data = data_bag_item("vhosts_ROR",site)
+  app_name = site_data["appname"]
+  document_root = "/u01/apps/qwinix/#{app_name}/current/public/"
+  errolog = "/u01/logs/httpd/#{app_name}-error.log"
+  customlog = "/u01/logs/httpd/#{app_name}-access.log"
+
+  template "/etc/httpd/conf.d/#{app_name}.conf" do
+    source "custom-vhosts-ror.erb"
+    mode "0644"
+    variables(
+      :appname => site_data["appname"],
+      :port => site_data["port"],
+      :server_name => site_data["servername"],
+      :document_root => document_root,
+      :passenger_mod => site_data["passenger_mod"],
+      :passenger_ruby_home => site_data["passenger_ruby_home"],
+      :passenger_pool_size => site_data["passenger_pool_size"],
+      :passenger_pool_id => site_data["passenger_pool_id"],
+      :passenger_maxinstance_app => site_data["passenger_maxinstance_app"],  
+      :rails_env => site_data["rails_env"]
+    )
+   notifies :restart, "service[httpd]"
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+end
