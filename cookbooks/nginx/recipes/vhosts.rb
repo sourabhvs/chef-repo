e5b154a8b921be6da2a31a68e0f58afe85c5aa51
pmprasad <pmprasad@qwinixtech.com>
2015-12-29 16:07:59 +0530

    update nginx data bag

---
+++ cookbooks/nginx/recipes/vhosts.rb
@@ -0,0 +1,29 @@
+execute 'delete conf.d/*.*' do
+     command 'rm -rf /etc/nginx/conf.d/*'
+end
+
+#Data bag for comman vhosts
+data_bag("nginx_vhosts").each do |site|
+  site_data = data_bag_item("nginx_vhosts",site)
+  app_name = site_data["appname"]
+  root_path = "/u01/apps/qwinix/#{app_name}/current/public"
+  upstream_path = "/u01/apps/qwinix/#{app_name}/shared/tmp/sockets/#{app_name}.sock"
+
+  template "/etc/nginx/conf.d/#{app_name}.conf" do
+    source "custom-nginx-vhosts.erb"
+    mode "0644"
+    variables(
+      :appname => app_name,
+      :server_name => site_data["servername"],
+      :root_path => root_path,
+      :upstream_path => upstream_path,
+      :listen => site_data["listen"],
+    )
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+end
